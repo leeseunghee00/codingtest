a668e360b246fb7d0c21b76482488f6a123ed402
Seunghee Lee <lsh9246@gmail.com>
2022-09-22 01:15:04 +0900

    test

--- C02/ex01/ft_strncpy.c
+++ C02/ex01/ft_strncpy.c
@@ -1,16 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ft.c                                            :+:      :+:    :+:   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: seunghel <seunghel@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/29 02:29:09 by seunghel          #+#    #+#             */
-/*   Updated: 2022/08/29 16:55:32 by seunghel         ###   ########.fr       */
+/*   Created: 2022/08/30 01:55:52 by seunghel          #+#    #+#             */
+/*   Updated: 2022/08/30 11:34:00 by seunghel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_ft(int *nbr)
+#include <direct.h>
+#include <stdio.h>
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
-	*nbr = 42;
+	unsigned int	i;
+
+	i = 0;
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while(i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
+
+int main(){
+	char test[] = "test";
+	char str[10];
+
+	printf("%s", ft_strncpy(str, test, 2));
 }
