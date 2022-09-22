ad55af85f181c12a0401d3933593864beff7a478
Seunghee Lee <lsh9246@gmail.com>
2022-09-22 23:40:15 +0900

    별 찍기 - 1

--- 2438.c
+++ 2438.c
@@ -1,12 +1,11 @@
-/*올렸던 문제*/
 #include <stdio.h>
 
-int main(){
+int main() {
 	int n;
 
 	scanf("%d", &n);
-	for(int i=0; i<n; i++){
-		for(int j=0; j<=i; j++)
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j <= i; j++)
 			printf("*");
 		printf("\n");
 	}
