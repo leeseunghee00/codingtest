0f3ab523a6c6caed7ff7bde953df0267707b17f3
leeseunghee00 <lsh9246@gmail.com>
2024-04-02 12:33:51 +0900

    feat: 문자열 내림차순으로 배치하기

---
+++ Java/src/main/java/leeseunghee/study/programmers/pgs_문자열내림차순으로배치하기.java
@@ -0,0 +1,25 @@
+package leeseunghee.study.programmers;
+
+import java.util.Arrays;
+
+public class pgs_문자열내림차순으로배치하기 {
+
+    public static void main(String[] args) {
+
+        System.out.println(solution("Zbcdefg"));
+    }
+
+    private static String solution(String s) {
+
+        String answer = "";
+        char[] str = s.toCharArray();
+
+        Arrays.sort(str);
+
+        answer = new StringBuilder(new String(str))
+                .reverse()
+                .toString();
+
+        return answer;
+    }
+}
