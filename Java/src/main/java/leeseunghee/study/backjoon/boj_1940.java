9b55192c5c7c8f6db30c21e93e4bb3d195f0db73
leeseunghee00 <lsh9246@gmail.com>
2024-04-05 20:59:34 +0900

    feat: 주몽 / 34824KB / 684ms

---
+++ Java/src/main/java/leeseunghee/study/backjoon/boj_1940.java
@@ -0,0 +1,31 @@
+package leeseunghee.study.backjoon;
+
+import java.util.Scanner;
+
+public class boj_1940 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int count = 0;
+
+        for (int i = 0; i < N; i++){
+            for (int j = i+1; j < N; j++) {
+                if (arr[i] + arr[j] == M) {
+                    count++;
+                }
+            }
+        }
+
+        System.out.println(count);
+    }
+}
