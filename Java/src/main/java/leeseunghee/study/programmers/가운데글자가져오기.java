5e56083afbf7919b17ba8a4cb8b6d87f1c0bbc1f
leeseunghee00 <lsh9246@gmail.com>
2024-04-11 21:48:07 +0900

    feat: 가운데 글자 가져오기

---
+++ Java/src/main/java/leeseunghee/study/programmers/가운데글자가져오기.java
@@ -0,0 +1,27 @@
+package leeseunghee.study.programmers;
+
+public class 가운데글자가져오기 {
+
+    public static void main(String[] args) {
+
+        String s1 = "abcde";
+        String s2 = "qwer";
+
+        System.out.println(solution(s1));
+        System.out.println(solution(s2));
+    }
+
+    private static String solution (String s) {
+
+        String answer = "";
+        int length = s.length();
+
+        if (length % 2 == 0) {
+            answer = s.substring(length / 2 - 1, length / 2 + 1);
+        } else {
+            answer = s.substring(length / 2, length / 2 + 1);
+        }
+
+        return answer;
+    }
+}
