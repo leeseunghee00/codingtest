d3ee42e516216721e405fb4ef3f19fef27200371
leeseunghee00 <lsh9246@gmail.com>
2024-04-11 22:29:11 +0900

    feat: 콜라 문제

---
+++ Java/src/main/java/leeseunghee/study/programmers/pgs_콜라문제.java
@@ -0,0 +1,22 @@
+package leeseunghee.study.programmers;
+
+public class pgs_콜라문제 {
+
+    public static void main(String[] args) {
+
+        System.out.println(solution(2, 1, 20));
+        System.out.println(solution(3, 1, 20));
+    }
+
+    private static int solution(int a, int b, int n) {
+
+        int answer = 0;
+
+        while (n >= a) {
+            answer += (n / a) * b;
+            n = (n / a) * b + (n % a);
+        }
+
+        return answer;
+    }
+}
