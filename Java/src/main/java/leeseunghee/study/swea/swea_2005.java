fb9aa2c2e4407daf2296c3342c4c25c37cb595f5
leeseunghee00 <lsh9246@gmail.com>
2024-05-19 02:51:08 +0900

    feat: 파스칼의 삼각형 / 26904KB / 136ms

---
+++ Java/src/main/java/leeseunghee/study/swea/swea_2005.java
@@ -0,0 +1,43 @@
+package leeseunghee.study.swea;
+
+import java.util.Scanner;
+
+public class swea_2005 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+
+		for (int test_case = 1; test_case <= T; test_case++) {
+			int N = sc.nextInt();
+			int[][] A = new int[N + 1][N + 1];
+
+			// 첫 번째 수 초기화
+			for (int i = 0; i < N; i++) {
+				A[0][i] = 1;
+			}
+
+			// 나머지 구하기
+			for (int i = 1; i < N; i++) {
+				for (int j = 0; j <= i; j++) {
+					if (j == 0 || j == i) {
+						A[i][j] = 1;
+					} else {
+						A[i][j] = A[i - 1][j - 1] + A[i - 1][j];
+					}
+				}
+			}
+
+			// 출력
+			System.out.println("#" + test_case);
+
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j <= i; j++) {
+					System.out.print(A[i][j] + " ");
+				}
+				System.out.println();
+			}
+		}
+	}
+}
