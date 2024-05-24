a8d741668777d61a22ec070a3bd7021e23e0db0b
Seunghee Lee <87460638+leeseunghee00@users.noreply.github.com>
2024-05-24 16:31:07 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.37 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution {
+    public long solution(long n) {
+        String[] str = String.valueOf(n).split("");
+
+		Arrays.sort(str);
+
+		StringBuilder sb = new StringBuilder();
+		
+		for (String s : str) {
+			sb.append(s);
+		}
+		
+		return Long.parseLong(sb.reverse().toString());
+    }
+}
