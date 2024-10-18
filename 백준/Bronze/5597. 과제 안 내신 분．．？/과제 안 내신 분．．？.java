9c147b1d21e0e756849663438b81d8894c1f06d4
Seunghee Lee <87460638+leeseunghee00@users.noreply.github.com>
2024-10-18 21:24:55 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 176 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+	static boolean[] students;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		students = new boolean[31];
+
+		for (int i = 0; i < 28; i++) {
+			int num = sc.nextInt();
+			students[num] = true;
+		}
+
+		for (int i = 1; i <= 30; i++) {
+			if (!students[i]) {
+				System.out.println(i);
+			}
+		}
+	}
+}
