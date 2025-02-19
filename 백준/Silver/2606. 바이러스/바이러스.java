78fd14a7711da9f659b9367123db801a9791b11b
Seunghee Lee <87460638+leeseunghee00@users.noreply.github.com>
2025-02-19 15:38:07 +0900

    [Silver III] Title: 바이러스, Time: 108 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,48 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    
-	static int N, M, count;
-	static boolean[] visit;
-	static ArrayList<ArrayList<Integer>> computer;
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+     static int N, T, result;
+     static boolean[] visited;
+     static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
 
-		N = sc.nextInt();
-		M = sc.nextInt();
-		visit = new boolean[N + 1];
-		computer = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
 
-		for (int i = 0; i <= N; i++) {
-			computer.add(new ArrayList<>());
-		}
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		for (int i = 0; i < M; i++) {
-			int a = sc.nextInt();
-			int b = sc.nextInt();
+        N = Integer.parseInt(br.readLine());
+        T = Integer.parseInt(br.readLine());
+        visited = new boolean[N + 1];
 
-			// 양방향 연결
-			computer.get(a).add(b);
-			computer.get(b).add(a);
-		}
+        for (int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
+        }
 
-		dfs(1);
-        
-		System.out.println(count);
-	}
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
 
-	private static void dfs(int v) {
-		visit[v] = true;
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
 
-		for (int k = 0; k < computer.get(v).size(); k++) {
-			int i = computer.get(v).get(k);
+        dfs(1);
 
-			if (!visit[i]) {
-				count++;
-				dfs(i);
-			}
-		}
-	}
+        System.out.println(result);
+    }
+
+    private static void dfs(int v) {
+
+        visited[v] = true;
+
+        if (graph.get(v).isEmpty()) {
+            return;
+        }
+
+        for (int i : graph.get(v)) {
+            if (!visited[i]) {
+                result += 1;
+                dfs(i);
+            }
+        }
+    }
 }
