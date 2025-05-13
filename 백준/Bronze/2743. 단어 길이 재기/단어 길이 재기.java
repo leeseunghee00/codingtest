eb0843d0557bd6ccba8de92152c3330c27c42fd6
Seunghee Lee <87460638+leeseunghee00@users.noreply.github.com>
2025-05-13 23:01:12 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 168 ms, Memory: 17664 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	static String str;
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		str = sc.nextLine();
+
+		System.out.println(str.length());
+	}
+}
