7a94b77c939399e9a96b65655d89c35d3d5b0c88
Seunghee Lee <87460638+leeseunghee00@users.noreply.github.com>
2025-05-15 22:00:40 +0900

    [Silver V] Title: 숫자 카드, Time: 828 ms, Memory: 135036 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int N, M;
+	static Map<Integer, Integer> map = new HashMap<>();
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		N = Integer.parseInt(br.readLine());
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++)  {
+			int num = Integer.parseInt(st.nextToken());
+			map.put(num, 1);
+		}
+
+		M = Integer.parseInt(br.readLine());
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < M; i++) {
+			int num = Integer.parseInt(st.nextToken());
+
+			if (map.containsKey(num)) {
+				sb.append("1 ");
+			} else {
+				sb.append("0 ");
+			}
+		}
+
+		System.out.println(sb);
+	}
+}
