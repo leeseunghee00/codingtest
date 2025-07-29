200247e900a3142058d90d44c569ad570a2001de
Seunghee Lee <87460638+leeseunghee00@users.noreply.github.com>
2025-07-29 23:15:06 +0900

    [Silver I] Title: 현대모비스 소프트웨어 아카데미, Time: 408 ms, Memory: 30900 KB -BaekjoonHub

---
+++ 백준/Silver/26091. 현대모비스 소프트웨어 아카데미/현대모비스 소프트웨어 아카데미.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int N, M, count;
+	static int[] arr;
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(arr);
+
+		int low = 0, high = N - 1;
+
+		while (low < high) {
+			if (arr[low] + arr[high] >= M) {
+				low++;
+				high--;
+				count++;
+			} else {
+				low++;
+			}
+		}
+
+		System.out.println(count);
+	}
+}
