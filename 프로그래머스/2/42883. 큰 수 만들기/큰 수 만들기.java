a176895bb2a7d98cea1b6009e123ba3adb5a7966
Seunghee Lee <87460638+leeseunghee00@users.noreply.github.com>
2025-09-09 19:08:10 +0900

    [level 2] Title: 큰 수 만들기, Time: 90.71 ms, Memory: 84.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    
+    public String solution(String number, int k) {
+        
+        Stack<Character> stack = new Stack<>();
+        
+        for (char num : number.toCharArray()) {
+            while (!stack.isEmpty() && k > 0 && stack.peek() < num) {
+                stack.pop();
+                k--;
+            }
+            stack.push(num);
+        }
+        
+        if (k > 0) {
+            stack.pop();
+            k--;
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for (char num : stack) {
+            sb.append(num);
+        }
+        
+        return sb.toString();
+    }
+}
