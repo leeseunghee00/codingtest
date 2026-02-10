17b581d70bbcc1007196f8dbe2391c1dd6452763
Seunghee Lee <87460638+leeseunghee00@users.noreply.github.com>
2026-02-10 21:30:51 +0900

    [Gold IV] Title: 동전 1, Time: 116 ms, Memory: 14368 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int N, K;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+
+		int[] num = new int[N + 1];
+
+		for (int i = 1; i <= N; i++) {
+			num[i] = Integer.parseInt(br.readLine());
+		}
+
+		Arrays.sort(num);
+
+		int[] dp = new int[K + 1];
+		dp[0] = 1;
+
+		for (int i = 1; i <= N; i++) {
+			for (int j = num[i]; j <= K; j++) {
+				dp[j] += dp[j - num[i]];
+			}
+		}
+
+		System.out.println(dp[K]);
+	}
+}
