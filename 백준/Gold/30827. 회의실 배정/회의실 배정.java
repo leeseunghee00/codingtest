1216e6183f712c8168a54a515d3f022c78fadb7e
Seunghee Lee <87460638+leeseunghee00@users.noreply.github.com>
2026-03-26 22:57:01 +0900

    [Gold IV] Title: 회의실 배정, Time: 1100 ms, Memory: 83372 KB -BaekjoonHub

--- 백준/Gold/30827. 회의실 배정/회의실 배정.java
+++ 백준/Gold/30827. 회의실 배정/회의실 배정.java
@@ -1,64 +1,68 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    
-    static class Meeting implements Comparable<Meeting>{
-        int from, to;
-        public Meeting(int from, int to) {
-            this.from = from;
-            this.to = to;
-        }
-        
-        @Override
-        public int compareTo(Meeting o) {
-            if (this.to == o.to) {
-                return Integer.compare(this.from, o.from);
-            } else return Integer.compare(this.to, o.to);
-        }
-    }
-    
-    static int K, N;
-    static int answer;
-    static Meeting[] meetings;
-    
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-        meetings = new Meeting[N];
-        for (int n=0; n<N; n++) {
-            st = new StringTokenizer(br.readLine());
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
-            meetings[n] = new Meeting(from,to);
-        }
-        
-        Arrays.sort(meetings);
-        ArrayList<Integer> rooms = new ArrayList<>();
-        
-        for (Meeting m : meetings) {
-            boolean flag = false;
-            
-            Collections.sort(rooms, (o1,o2) -> -Integer.compare(o1,o2));
-            
-            for (int n=0; n<rooms.size(); n++) {
-                if (rooms.get(n) < m.from) {
-                    rooms.set(n,m.to);
-                    answer++;
-                    flag = true;
-                    break;
-                }
-            }
-            
-            if (flag == false && rooms.size() < K) {
-                rooms.add(m.to);
-                answer++;
-            }
-        }
-        
-        System.out.println(answer);
-        
-    }
+
+	static int N, K, count;
+	static Meeting[] meetings;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		meetings = new Meeting[N];
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+
+			meetings[i] = new Meeting(start, end);
+		}
+
+		Arrays.sort(meetings);
+		ArrayList<Integer> rooms = new ArrayList<>();
+
+		for (Meeting meeting : meetings) {
+			boolean flag = false;
+
+			rooms.sort((o1, o2) -> -Integer.compare(o1, o2));
+
+			for (int i = 0; i < rooms.size(); i++) {
+				if (rooms.get(i) < meeting.start) {
+					rooms.set(i, meeting.end);
+					count++;
+					flag = true;
+					break;
+				}
+			}
+
+			if (!flag && rooms.size() < K) {
+				rooms.add(meeting.end);
+				count++;
+			}
+		}
+
+		System.out.println(count);
+	}
+
+	static class Meeting implements Comparable<Meeting> {
+		int start;
+		int end;
+
+		public Meeting(int start, int end) {
+			this.start = start;
+			this.end = end;
+		}
+
+		@Override
+		public int compareTo(Meeting obj) {
+			if (this.end == obj.end) {
+				return Integer.compare(this.start, obj.start);
+			}
+			return Integer.compare(this.end, obj.end);
+		}
+	}
 }
